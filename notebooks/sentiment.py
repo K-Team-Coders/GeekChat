6c24d984e83dbffa8235ab54d8e99b684ba278f1
qTemiO <71713499+qTemiO@users.noreply.github.com>
2024-04-26 20:22:30 +0300

    added sentiment feature

---
+++ notebooks/sentiment.py
@@ -0,0 +1,25 @@
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+model_checkpoint = 'cointegrated/rubert-tiny-sentiment-balanced'
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+model = AutoModelForSequenceClassification.from_pretrained(model_checkpoint)
+if torch.cuda.is_available():
+    model.cuda()
+    
+def get_sentiment(text, return_type='label'):
+    """ Calculate sentiment of a text. `return_type` can be 'label', 'score' or 'proba' """
+    with torch.no_grad():
+        inputs = tokenizer(text, return_tensors='pt', truncation=True, padding=True).to(model.device)
+        proba = torch.sigmoid(model(**inputs).logits).cpu().numpy()[0]
+    if return_type == 'label':
+        return model.config.id2label[proba.argmax()]
+    elif return_type == 'score':
+        return proba.dot([-1, 0, 1])
+    return proba
+
+text = 'всем привет'
+# classify the text
+print(get_sentiment(text, 'label'))
+
+# ВЫВЕДЕТ либо нейтрал либо негатив либо позитив. Для нейтралов подавать команду "НЕ СЧИТАТЬ!"
